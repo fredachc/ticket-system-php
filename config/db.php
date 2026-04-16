391d0aa8b6e7fa91f7a32f8936e364e93e368197
fredachc <karenchoy3@yahoo.com.hk>
2026-04-16 14:48:51 +0800

    Add files via upload

--- config/db.php
+++ config/db.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "ticket_system";
+$user = "root";
+$pass = "";
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("DB Connection failed: " . $e->getMessage());
+}
+?>
